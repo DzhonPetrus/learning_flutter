0d6c9de91962b60a5f8b709720a211ae5b7d078f
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-10-02 02:23:48 +0800

    login added, second screen added

---
+++ lib/dashboard.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  Dashboard({Key? key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Dashboard')),
+      body: Center(
+        child: Text('Welcome to Dashboard!'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dashboard.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First Flutter App',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.amber,
       ),
       home: const MyHomePage(title: 'First Flutter App'),
     );
@@ -30,14 +32,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    var _username = _usernameController.text;
+    var _password = _passwordController.text;
+    print('''
+    Username: $_username
+    Password: $_password
+    ''');
+
+    if(_username == 'admin' && _password == 'password') {
+      Navigator.push(
+        context, 
+        MaterialPageRoute(
+          builder: (context) => Dashboard(),
+        )
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('First Flutter App')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              width:200,
+              height:200,
+              child: Image.asset('assets/JUAN_TEKNOLOHIYA.png')
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 70,
+                right: 70,
+                bottom: 10,
+              ),
+              child: TextFormField(
+                controller: _usernameController,
+                style: const TextStyle(
+                  color:Colors.black,
+                  fontSize: 16,
+                ),
+                cursorColor: Colors.black,
+                decoration: const InputDecoration(
+                  hintText: 'Input Username',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 70,
+                right: 70,
+                bottom: 10,
+              ),
+              child: TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                style: const TextStyle(
+                  color:Colors.black,
+                  fontSize: 16,
+                ),
+                cursorColor: Colors.black,
+                decoration: const InputDecoration(
+                  hintText: 'Input Password',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35.0),
+                color: Colors.amber,
+              ),
+              child: MaterialButton(
+                onPressed: _login,
+                child: const Text('Login'),
+              ),
+            ),
+            
 
           ],
         ),
