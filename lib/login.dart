58eac03d87979c2ec9c131439977ba0f16c24f3f
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-10-03 00:34:35 +0800

    MyHomePage -> LoginPage, page separated from main.dart

---
+++ lib/login.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    var _username = _usernameController.text;
+    var _password = _passwordController.text;
+
+    // print('''
+    // Username: $_username
+    // Password: $_password
+    // ''');
+
+    if(_username == 'admin' && _password == 'password') {
+      Navigator.push(
+        context, 
+        MaterialPageRoute(
+          builder: (context) => const Dashboard(),
+        )
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Flutter App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width:200,
+              height:200,
+              child: Image.asset('assets/JUAN_TEKNOLOHIYA.png')
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 70,
+                right: 70,
+                bottom: 10,
+              ),
+              child: TextFormField(
+                controller: _usernameController,
+                style: const TextStyle(
+                  color:Colors.black,
+                  fontSize: 16,
+                ),
+                cursorColor: Colors.black,
+                decoration: const InputDecoration(
+                  hintText: 'Input Username',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 70,
+                right: 70,
+                bottom: 10,
+              ),
+              child: TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                style: const TextStyle(
+                  color:Colors.black,
+                  fontSize: 16,
+                ),
+                cursorColor: Colors.black,
+                decoration: const InputDecoration(
+                  hintText: 'Input Password',
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35.0),
+                color: Colors.amber,
+              ),
+              child: MaterialButton(
+                onPressed: _login,
+                child: const Text('Login'),
+              ),
+            ),
+            
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'dashboard.dart';
+import 'login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,122 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: const MyHomePage(title: 'First Flutter App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  void _login() {
-    var _username = _usernameController.text;
-    var _password = _passwordController.text;
-
-    // print('''
-    // Username: $_username
-    // Password: $_password
-    // ''');
-
-    if(_username == 'admin' && _password == 'password') {
-      Navigator.push(
-        context, 
-        MaterialPageRoute(
-          builder: (context) => const Dashboard(),
-        )
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('First Flutter App')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width:200,
-              height:200,
-              child: Image.asset('assets/JUAN_TEKNOLOHIYA.png')
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 70,
-                right: 70,
-                bottom: 10,
-              ),
-              child: TextFormField(
-                controller: _usernameController,
-                style: const TextStyle(
-                  color:Colors.black,
-                  fontSize: 16,
-                ),
-                cursorColor: Colors.black,
-                decoration: const InputDecoration(
-                  hintText: 'Input Username',
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 70,
-                right: 70,
-                bottom: 10,
-              ),
-              child: TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                style: const TextStyle(
-                  color:Colors.black,
-                  fontSize: 16,
-                ),
-                cursorColor: Colors.black,
-                decoration: const InputDecoration(
-                  hintText: 'Input Password',
-                  border: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width / 1.3,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(35.0),
-                color: Colors.amber,
-              ),
-              child: MaterialButton(
-                onPressed: _login,
-                child: const Text('Login'),
-              ),
-            ),
-            
-
-          ],
-        ),
-      ),
+      home: const LoginPage(title: 'First Flutter App'),
     );
   }
 }
