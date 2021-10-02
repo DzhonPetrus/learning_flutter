88b790c2022d2e296cf0142f7480718cb06edae4
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-10-03 02:01:47 +0800

    wordpair added

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -58,7 +58,7 @@ class _DashboardState extends State<Dashboard> {
                       Navigator.push(
                         context, 
                         MaterialPageRoute(
-                          builder: (context) => const WordPair(),
+                          builder: (context) => const WordPairPage(),
                         )
                       );
                     },
